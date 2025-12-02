8cb757ddd70d6242647d2e6cbfdfda9ae4ffe74d
lime0x001 <mohammada4raf@gmail.com>
2025-12-02 19:53:27 +0200

    feat: add checkout endpoint and improve event retrieval logic

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,13 +1,27 @@
-import { sendSuccess } from '../utils/response.js';
+import { sendSuccess, sendFail } from '../utils/response.js';
 import asyncWrapper from '../middlewares/asyncWrapper.js';
 import eventService from '../services/eventService.js';
 
 const eventController = {
     show: asyncWrapper(async (req, res) => {
-        const { organizerId, eventSlug: slug } = req.params;
+        const { id } = req.params;
 
-        const result = await eventService.show(organizerId, slug);
+        const result = await eventService.show(id);
+
+        if (!result || result.status === 'fail') {
+            return sendFail(res, result.data, result.statusCode || 400);
+        }
         sendSuccess(res, { event: result }, 200);
+    }),
+
+    checkout: asyncWrapper(async (req, res) => {
+        const { id: userId, email: userEmail } = req.user;
+        const { id } = req.params;
+        const { tickets } = req.body;
+
+        const result = await eventService.checkout(id, userId, userEmail, tickets);
+
+        sendSuccess(res, result.data, 201);
     }),
 };
 
--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -1,10 +1,23 @@
 import express from 'express';
-
+import validate from '../middlewares/validate.js';
+import auth from '../middlewares/auth.js';
+import { paymentLimiter } from '../middlewares/rateLimiter.js';
 import eventValidation from '../validations/eventValidation.js';
 import eventController from '../controllers/eventController.js';
 
 const Router = express.Router();
 
-Router.get('/:organizerId/:eventSlug', eventValidation.show, eventController.show);
+Router.get('/:id', eventValidation.show, validate, eventController.show);
+
+//! express.raw();
+
+Router.post(
+    '/:id/checkout',
+    paymentLimiter,
+    auth,
+    eventValidation.checkout,
+    validate,
+    eventController.checkout
+);
 
 export default Router;
