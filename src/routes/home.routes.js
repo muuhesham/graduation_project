387e8bb4ac1e2c83bc9486f910515c4f736c7bbf
Ali-Magdy <alimagdye1@gmail.com>
2025-12-07 03:35:34 +0200

    Add nearby, and personalized events

--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { publicLimiter } from '../middlewares/rateLimiter.js';
 import homeController from '../controllers/homeController.js';
-import auth from '../middlewares/auth.js';
+import optionalAuth from '../middlewares/optionalAuth.js';
 
 const Router = express.Router();
 
@@ -13,8 +13,8 @@ Router.get('/categories', publicLimiter, homeController.allCategories);
 
 Router.get('/past-events', publicLimiter, homeController.pastEventsAndHighlights);
 
-Router.get('/nearby-events', publicLimiter, auth, homeController.nearbyEvents);
+Router.get('/nearby-events', publicLimiter, optionalAuth, homeController.nearbyEvents);
 
-Router.get('/personalized-events', publicLimiter, auth, homeController.personalizedEvents);
+Router.get('/personalized-events', publicLimiter, optionalAuth, homeController.personalizedEvents);
 
 export default Router;
