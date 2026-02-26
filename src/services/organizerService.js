1dbe21efff1fdb6e1ff0a986f08f2772efef40df
muuhesham <muhammedhesham488@gmail.com>
2026-02-26 15:45:18 +0200

    fix functions in orginizer service to apply soft delete and connect to new middleware

--- src/services/organizerService.js
+++ src/services/organizerService.js
@@ -222,13 +222,6 @@ const organizerService = {
             };
         }
 
-        if (event.deletedAt) {
-            return {
-                status: 'fail',
-                data: { error: 'Event already deleted' },
-            };
-        }
-
         // check if the event related to tickets -> can't delete else -> soft delete or hard delete
 
         let result;
@@ -260,7 +253,7 @@ const organizerService = {
     },
 
     async getByUserId(userId) {
-        return prismaClient.organizer.findUnique({
+        return prismaClient.organizer.findFirst({
             where: { userId },
         });
     },
