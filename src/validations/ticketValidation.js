eeb708fc176d039b865829f205073227123242b9
muuhesham <muhammedhesham488@gmail.com>
2026-03-05 06:18:05 +0200

    add new validations to order & ticket to handle any erros in body or params

---
+++ src/validations/ticketValidation.js
@@ -0,0 +1,11 @@
+import { param } from 'express-validator';
+
+const ticketValidation = {
+    getSingleTicket: [
+        param('id')
+            .notEmpty().withMessage('Ticket ID is required')
+            .isUUID().withMessage('Invalid Ticket ID format'),
+    ],
+};
+
+export default ticketValidation;
