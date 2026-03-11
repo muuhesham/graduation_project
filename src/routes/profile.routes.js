becb83ca487b5bb326a5baa32c3668ee8625d5ed
muuhesham <muhammedhesham488@gmail.com>
2026-03-12 01:14:16 +0200

    add new routes attended-events , preferences and change-preferences

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -7,11 +7,14 @@ import { profileLimiter } from "../middlewares/rateLimiter.js";
 
 const Router = express.Router();
 
-Router.get('/me', auth, profileController.getMyProfile);
-Router.patch('/me', auth, profileValidations.updateMyProfile, validate, profileLimiter, profileController.updateMyProfile);
-Router.delete('/me', auth, profileController.deleteMyProfile);
-Router.patch('/me/change-password', auth, profileValidations.updatePassword, validate, profileLimiter, profileController.updatePassword);
-Router.patch('/me/change-email', auth, profileValidations.updateEmail, validate, profileLimiter, profileController.updateEmail);
-Router.get('/confirm-email', profileController.confirmEmailUpdate);
+Router.get('/', auth, profileController.getMyProfile);
+Router.patch('/', auth, profileValidations.updateMyProfile, validate, profileLimiter, profileController.updateMyProfile);
+Router.delete('/', auth, profileController.deleteMyProfile);
+Router.patch('/change-password', auth, profileValidations.updatePassword, validate, profileLimiter, profileController.updatePassword);
+Router.patch('/change-email', auth, profileValidations.updateEmail, validate, profileLimiter, profileController.updateEmail);
+Router.get('/confirm-email', profileValidations.confirmEmail, validate, profileController.confirmEmailUpdate);
+Router.get('/attended-events', auth, profileController.getAttendEvents);
+Router.get('/preferences', auth, profileController.getPreferences);
+Router.patch('/change-preferences', auth, profileValidations.updatePreferences, validate, profileController.updatePreferences);
 
 export default Router;
