bd8888e8d83c20eac02123e3cd01ec39a3be646c
muuhesham <muhammedhesham488@gmail.com>
2026-05-04 23:17:50 +0300

    update socket config with new rooms for users & organizers

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,4 +1,5 @@
 import { Server } from 'socket.io';
+import { prisma as prismaClient } from '../config/db.js';
 import chatbotService from '../services/chatbotService.js';
 import { validateChat } from '../validations/chatbotValidtion.js';
 import { socketAuth } from '../middlewares/socketauth.js';
@@ -16,18 +17,38 @@ export function initSocket(server) {
 
     io.use(socketAuth);
 
-    io.on('connection', (socket) => {
+    io.on('connection', async(socket) => {
         console.log('🟢 Client connected:', socket.id);
         console.log('User ID:', socket.userId);
-        
-        
-        // Join event room
+
+        // ===== NOTIFICATION ROOMS =====
+        // Join user notification room
+        socket.join(`user-${socket.userId}`);
+        console.log(`Socket ${socket.id} joined user-${socket.userId}`);
+
+        const organizer = await prismaClient.organizer.findUnique({
+        where: { userId: socket.userId },
+        select: { id: true },
+        });
+
+        if(organizer) {
+            // join organizer notification room
+                socket.join(`organizer-${organizer.id}`);
+                console.log(`Socket ${socket.id} joined organizer-${organizer.id}`);
+        }
+
+        // ===== EVENT ROOMS =====
         socket.on('join-event', (eventId) => {
             socket.join(`event-${eventId}`);
             console.log(`📌 Socket ${socket.id} joined event-${eventId}`);
         });
-        
-        
+
+        socket.on('leave-event', (eventId) => {
+            socket.leave(`event-${eventId}`);
+            console.log(`Socket ${socket.id} left event-${eventId}`);
+        });
+
+        // ===== CHATBOT FUNCTIONALITY =====
         // chatbot room
         socket.on('chatbot-message', async (data) => {
             const { message } = data;
@@ -37,7 +58,10 @@ export function initSocket(server) {
                 validateChat({ message, socket });
                 socket.emit('chatbot-reply', { isTyping: true });
                 const response = await chatbotService.handleChat({ message, userId });
-                socket.emit('chatbot-reply', { message: response, timeStamp: new Date().toISOString() });
+                socket.emit('chatbot-reply', {
+                    message: response,
+                    timeStamp: new Date().toISOString(),
+                });
             } catch (err) {
                 console.error('Chatbot Error:', err);
                 socket.emit('chatbot-reply', {
