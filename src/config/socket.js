0173f1c540fce695fa7ce34e0c6fe1c1da314fcb
muuhesham <muhammedhesham488@gmail.com>
2026-05-10 18:51:52 +0300

    deny admins from rooms and allow guests & remove leave-event room

--- src/config/socket.js
+++ src/config/socket.js
@@ -2,7 +2,7 @@ import { Server } from 'socket.io';
 import { prisma as prismaClient } from '../config/db.js';
 import chatbotService from '../services/chatbotService.js';
 import { validateChat } from '../validations/chatbotValidtion.js';
-import { socketAuth } from '../middlewares/socketauth.js';
+import { socketOptionalAuth } from '../middlewares/socketOptionalAuth.js';
 import { chatLimiter } from '../middlewares/rateLimiter.js';
 
 export function initSocket(server) {
@@ -15,37 +15,35 @@ export function initSocket(server) {
         },
     });
 
-    io.use(socketAuth);
+    io.use(socketOptionalAuth);
 
-    io.on('connection', async(socket) => {
+    io.on('connection', async (socket) => {
         console.log('🟢 Client connected:', socket.id);
         console.log('User ID:', socket.userId);
 
         // ===== NOTIFICATION ROOMS =====
-        // Join user notification room
-        socket.join(`user-${socket.userId}`);
-        console.log(`Socket ${socket.id} joined user-${socket.userId}`);
+        if (socket.userId) {
+            // Join user notification room
+            socket.join(`user-${socket.userId}`);
+            console.log(`Socket ${socket.id} joined user-${socket.userId}`);
 
-        const organizer = await prismaClient.organizer.findUnique({
-        where: { userId: socket.userId },
-        select: { id: true },
-        });
+            const organizer = await prismaClient.organizer.findUnique({
+                where: { userId: socket.userId },
+                select: { id: true },
+            });
 
-        if(organizer) {
-            // join organizer notification room
+            if (organizer) {
                 socket.join(`organizer-${organizer.id}`);
                 console.log(`Socket ${socket.id} joined organizer-${organizer.id}`);
+            }
+        } else {
+            console.log(`Socket ${socket.id} connected without authenticated user`);
         }
 
         // ===== EVENT ROOMS =====
         socket.on('join-event', (eventId) => {
             socket.join(`event-${eventId}`);
             console.log(`📌 Socket ${socket.id} joined event-${eventId}`);
-        });
-
-        socket.on('leave-event', (eventId) => {
-            socket.leave(`event-${eventId}`);
-            console.log(`Socket ${socket.id} left event-${eventId}`);
         });
 
         // ===== CHATBOT FUNCTIONALITY =====
